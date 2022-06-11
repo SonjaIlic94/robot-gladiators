d6bf6199486b7e48678064d1288f2190b77204b2
sonjailic <sonjailic94@hotmail.com>
2022-06-11 14:39:44 -0400

    add robot name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
 window.alert("This is an alert! JavaScript is runnning!");
+
+var playerName = window.prompt("What is your robots name?");
+console.log(playerName);
+
+console.log("logs a strings");
+//this will do math
+console.log(10 + 10);
+
+console.log("Your robots name is " + playerName);
+
+function fight(){
+    window.alert("The fight has begun!")
+}
+
+//fight();
